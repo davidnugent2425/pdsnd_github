8663008874bfe58ed2fd38f5d32c3e4d72bbfe66
David Nugent <david.nugent2425@gmail.com>
2019-05-05 20:46:30 +0100

    Improved comment in bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -107,7 +107,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel in the chosen dataset."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
